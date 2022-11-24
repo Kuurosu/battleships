294713286b1cbdf25f3ab07dd26e618d29fa6913
Rob Sizeland <robcakes69@gmail.com>
2022-11-24 16:18:10 +0000

    Change board to a class. Creat temp run_game to test Board class play_board function

--- run.py
+++ run.py
@@ -9,23 +9,31 @@ print(scores)
 
 # Implement board
 # The board will be generated automatically in a 5x5 layout. 
-BOARD = []
-
-for i in range(5):
-    BOARD.append("O" * 5)
-
-def board_layout(BOARD):
+class Board:
     """
-    Creates the board layout so that each list item (00000) is on a new line and is spaced out evenly between each 
+    Creates a class of Board with a width and height of 5.
     """
-    for row in BOARD:
-        print(" ".join(row))
+    def __init__(self, board):
+        self.board = board
+    
+    def play_board(self):
+        print("  1 2 3 4 5")
+        row_number = 1
+        for row in self.board:
+            print(row_number, " ".join(row))
+            row_number += 1
 
-board_layout(BOARD)
 
 # Implement random int
 # A function will be created to handle the computer's choice for the 
 # integers it will choose. 
+
+def computer_random_int():
+    """
+    Using random, the function creates a random number from 0 through to 5.
+    """
+    computer_number = random.randint(0,5)
+    print(computer_number)
 
 # Implement check coords
 # After the computer and the player has made a choice, it will be passed
@@ -45,4 +53,8 @@ board_layout(BOARD)
 # Implement game logic
 
 # Implement new game
+def run_game():
+    player_board = Board(["~"] * 5 for i in range(5))
+    Board.play_board(player_board)
 
+run_game()
