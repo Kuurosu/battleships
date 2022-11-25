d46f16780a0b7468ae89f35036bca856a79d186e
Rob Sizeland <robcakes69@gmail.com>
2022-11-25 11:20:45 +0000

    Add user input to get the first row int and add to placement list

--- run.py
+++ run.py
@@ -27,24 +27,6 @@ class Board:
 # Implement random int
 # A function will be created to handle the computer's choice for the 
 # integers it will choose. 
-
-def computer_random_int():
-    """
-    Using random, the function creates a random number from 0 through to 5.
-    """
-    computer_number = random.randint(0,5)
-    return computer_number
-
-# Implement check coords
-# After the computer and the player has made a choice, it will be passed
-# into a coord check to see if it is:
-# Valid inside the game area.
-# Hasn't been previously chosen. 
-
-# Implement board population
-# This handles the board after relevant choices have been made and stores
-# it for future choices to show the player what has currently already been
-# chosen
 def computer_ships():
     """
     Randomly places the computers ships on the map within the game area.
@@ -58,15 +40,43 @@ def computer_ships():
         ship += 1
     print(SHIP_PLACEMENT)
 
-
 # Implement board guess
 # This handles the inputs from the user for which row and column they are 
 # choosing to play the game
-# def get_user_input():
-#     while True:
-#         if top_row < 6:
-#             top_row = input("Type the column you'd like to place your ship: ").strip()
-#             break
+def get_user_input():
+    user_ship_placement = []
+    row_guess = 0
+    col_guess = 0
+    while True:
+        print("\nWhat row would you like your first ship?\n")
+        # user_row = int(input("Please type a number from 1-5: "))
+        # user_row = int(user_row_str)
+        try:
+            user_row = int(input("Please type a number from 1-5: "))
+            if user_row < 6 and user_row > 0:
+                user_row - 1
+                user_ship_placement.append(user_row)
+                break
+            else:
+                print("\nThat is not a number within 1-5")
+                continue
+        except ValueError:
+            print("That is not a number within 1-5 or not a number. Please try again.\n")
+            continue
+    print(user_ship_placement)
+
+
+
+# Implement check coords
+# After the computer and the player has made a choice, it will be passed
+# into a coord check to see if it is:
+# Valid inside the game area.
+# Hasn't been previously chosen. 
+
+# Implement board population
+# This handles the board after relevant choices have been made and stores
+# it for future choices to show the player what has currently already been
+# chosen
 
 # Implement game logic
 
@@ -81,6 +91,7 @@ def run_game():
     player_board = Board(["~"] * 5 for i in range(5))
     Board.play_board(player_board)
     computer_ships()
+    get_user_input()
     
 
 run_game()
