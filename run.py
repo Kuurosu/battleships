080b4b6c4b7b80fdceca8b4afe0aca527e32b8f8
Rob Sizeland <robcakes69@gmail.com>
2022-11-28 18:39:52 +0000

    Fix user guess bug where it was + 1 to the number. Finish user ship input. Add guess board to run game

--- run.py
+++ run.py
@@ -55,18 +55,20 @@ class Ship:
         """
         Requests the user to enter the coordinates of where they would like to place their ships.
         """
+        print("\nWhere would you like your ships?")
         ship = 0
         while ship < 3:
             try:
-                print("\nWhere would you like your first ship?")
-                self.col = input("Choose a column from A-E: ").upper().strip()
+                self.col = input("\nChoose a column from A-E: ").upper().strip()
                 if self.col not in "ABCDE":
                     print("\nIt needs to be a letter within A-E")
                 self.col_one = Board.letter_to_number()[self.col]
                 self.row = int(input("Choose a row from 1-5: "))
-                while self.board[self.row][self.col_one] == "@":
+                self.row_one = self.row - 1
+                while self.board[self.row_one][self.col_one] == "?":
                     print("You've already chosen that coordinate")
-                self.board[self.row][self.col_one] = "@"
+                    return Ship.user_ship_input(self)
+                self.board[self.row_one][self.col_one] = "?"
             except ValueError and KeyError:
                 print("\nNot a valid coordinate. Please try again.")
                 return Ship.user_ship_input(self)
@@ -124,12 +126,15 @@ def run_game():
     """
     computer_board = Board([[" "] * 5 for i in range(5)])
     user_board = Board([[" "] * 5 for i in range(5)])
+    guess_board = Board([[" "] * 5 for i in range(5)])
     Ship.computer_ships(computer_board)
-    # Ship.user_ship_input(user_board)
+    Ship.user_ship_input(user_board)
 
     turns = 10
     while turns > 0:
+        Board.play_board(guess_board)
         Board.play_board(user_board)
+        Ship.get_computer_guess(user_board)
         # Uncomment the line below to show the computers board
         # Board.play_board(computer_board)
 
@@ -140,29 +145,29 @@ def run_game():
         # Ship.get_computer_guess(user_board)
 
         # Checks to see if user has already guessed the position
-        if user_board.board[user_row_input][user_col_input] == "O" or user_board.board[user_row_input][user_col_input] == "-":
+        if guess_board.board[user_row_input][user_col_input] == "O" or guess_board.board[user_row_input][user_col_input] == "-":
             print("You've already guessed that coordinate. Try again.")
             user_row_input, user_col_input = Ship.get_user_guess(object)
 
         # Checks the user input to see if a ship was hit
         if computer_board.board[user_row_input][user_col_input] == "X":
             print("\nYou sunk a battleship!")
-            user_board.board[user_row_input][user_col_input] = "O"
+            guess_board.board[user_row_input][user_col_input] = "O"
         else:
             print("\nYou missed!")
-            user_board.board[user_row_input][user_col_input] = "-"
+            guess_board.board[user_row_input][user_col_input] = "-"
             turns -= 1
             print(f"You have {turns} shots remaining!")
 
         # Check if all the ships have been hit
         if Ship.all_ships_hit(user_board) == 3:
-            print(f"\nCongratulations! You've wiped out all of the enemy ships!")
+            print("\nCongratulations! You've wiped out all of the enemy ships!")
+            print("\nYou Win!\n")
             break
 
         else:
             if turns == 0:
-                print("\nYou have run out of ammo and been overwhelmed by your defeat.")
-                print("\nYou lose.\n")
+                print("\nA valiant effort from both sides. But ammo and rations are required for the crew. Both sides retreat to resupply for another day.")
                 break
 
 
